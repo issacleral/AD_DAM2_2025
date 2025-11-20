96acb45d031d00181479c6e9e00aadec20408336
issacleral <156449350+issacleral@users.noreply.github.com>
2025-11-20 15:32:27 +0100

    pantalla de reinicio

--- GTA V/reiniciar.php
+++ GTA V/reiniciar.php
@@ -7,21 +7,15 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.cdnfonts.com/css/pricedown" rel="stylesheet">
-    <link rel="stylesheet" href="style/reiniciar.css">
     <title>Misión Reiniciada</title>
 </head>
 <body>
-    <div class="reset-container">
-        <h1>MISIÓN REINICIADA</h1>
-        <div class="wasted">WASTED</div>
-        <p>Volviendo al punto de inicio...</p>
-    </div>
-    
+    <video style="width: 100%;" src="video/gtav_1.mp4" autoplay muted loop></video>
     <script>
-        setTimeout(function() {
+        setTimeout(() => {
             window.location.href = 'index.php';
-        }, 3000);
+        }, 10000);
     </script>
+
 </body>
 </html>
